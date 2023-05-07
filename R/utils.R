e4cb04f3a72021bc8e72170a071a6dac8fbfac31
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2023-05-08 03:48:58 +0900

    get_gram_2d
    change from get_gram to get_gram_2d

---
+++ R/utils.R
@@ -0,0 +1,35 @@
+get_gram_2d <- function(argval, basis_evals){
+  n_basis <- dim(basis_evals)[2]
+  J <- matrix(nrow= n_basis, ncol = n_basis)
+  for (i in 1:n_basis) {
+    for (j in 1:n_basis){
+      J[i,j] <- pracma::trapz(argval, basis_evals[,i]*basis_evals[,j])
+    }}
+  return(J)
+}
+
+
+fit_spine_2d <- function(argvals, evals, n_basis){
+
+  # create 1d b-spline basis functions
+  PhiB <- splines2::bSpline(argvals, df=n_basis, degree = 3, intercept = TRUE) #Jt X M B spline Basis
+  cat(paste("use", n_basis, "basis functions\n"))
+
+  # calculate basis coefficients for observed functional data
+  # idea from Dr. Jang
+  C <- t(MASS::ginv(PhiB) %*% t(evals))
+
+  # calculate gram matrices for
+  # - basis functions (J)
+  # - 2nd order derivative of basis functions (J'')
+  J <- get_gram_2d(argvals, PhiB)
+  J_dotdot <- get_gram_2d(argvals, deriv(PhiB, 2))
+
+  return(
+    list(
+      C = C,
+      J = J,
+      J_dotdot = J_dotdot
+    )
+  )
+}
