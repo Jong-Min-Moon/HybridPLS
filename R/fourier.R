dbdddebc0b55646251c87bd9feafb477a75d7297
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2023-07-22 23:59:03 +0900

    implemented filter signal for many frequency

--- R/fourier.R
+++ R/fourier.R
@@ -1,29 +1,33 @@
 
-filter_signal <- function(signal.original, eval_point, frequency_now, gaussian_bandwidth){
-n_sample = nrow(signal.original)
-n_eval = ncol(signal.original)
-time        = eval_point;
-srate      = n_eval;
-wavelet_half_length = 19/n_eval
-time = seq(-wavelet_half_length, wavelet_half_length, length = 2* wavelet_half_length * n_eval)
-n_wavelet   = length(time);
-n_data  = length(time);
-n_convolution = n_data + n_wavelet - 1;
-n_half_wavelet = (n_wavelet - 1)/2
-filtered_analytic_signal = matrix(NA, nrow = n_sample, ncol = n_eval)
-
-for ( ii in 1:n_sample){
-  signal_now = signal.original[ii,]
-  fft_signal_now = fft(signal_now)
-  fft_signal_now_padded <- c(fft_signal_now, rep(0, n_convolution - n_data))
+filter_signal <- function(
+    gaussian_bandwidth, frequency_list,
+    signal.original, time,
+    n.wavelet.timepoint
+    ){
+  n.timepoint.original <- length(time)
+  srate <- n.timepoint.original/(time[n.timepoint.original] - time[1])
+  signal.concat <- c(t(signal.original))
+  n.timepoint.concat <- ncol(signal.concat)
+  n.timepoint.conv = n.timepoint.concat + n.timepoint.wavelet - 1;
 
 
-  wavelet_now_padded <- c(wavelet_now, rep(0, n_convolution - n_wavelet))
-  convolution_result_now   = ifft(fft_signal_now_padded * fft(wavelet_now_padded));
-  convolution_result_now = convolution_result_now[ (n_half_wavelet+1) : (n_convolution - n_half_wavelet)]
-  filtered_analytic_signal[ii, ] = Re(convolution_result_now);
-}
-return(filtered_analytic_signal)
+
+  wavelet.time.coverage <- n.timepoint.wavelet/n.timepoint.concat
+  time.wavelet <- seq(-wavelet.time.coverage/2, wavelet.time.coverage/2, length = n.timepoint.wavelet) # to ensure same srate
+
+  wavelet.mat <- complex_morlet_wavelet(gaussian_bandwidth, frequency_list, time.wavelet)
+  wavelet.mat.padded <- cbind(wavelet.mat, matrix(0, nrow = length(frequency_list), ncol = n.timepoint.conv - n.wavelet))
+  wavelet.fft <- fft.rowwize(wavelet.mat.padded)
+
+  signal.concat.fft <- fft(
+    c(signal.concat, rep(0, (n.timepoint.conv - n.timepoint.concat)))
+  )
+  signal.concat.fft <- matrix(rep(1, length(frequency_list))) %*% signal.concat.fft
+
+  conv.result <- ifft.rowwize(wavelet.fft * signal.concat.fft)
+  n.timepoint.wavelet.half = (n.timepoint.wavelet - 1)/2
+  conv.result = convolution_result_now[ (n.timepoint.wavelet.half+1) : (n.timepoint.conv - n.timepoint.wavelet.half)]
+  return(abs(conv.result))
 }
 
 
@@ -48,3 +52,5 @@ complex_morlet_wavelet <- function(gaussian_bandwidth, frequency_list, time){
 }
 
 
+fft.rowwize <- function(X) t(mvfft(t(X)))
+ifft.rowwize <- function(F) t(mvfft(t(F), inverse=TRUE)) / ncol(F)
